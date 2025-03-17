05b1135fff5bb9b84df1a0244e36456ecb25438f
Idace <idaebeling@gmail.com>
2025-03-17 14:15:17 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log("Hello World!")
+
+let name= "Ida";
+let age= "35";
+let favoriteColor="green";
+
+console.log(`By day, I'm just ${name}, an ordinary ${age} -year old. But when the sun sets... I become the Captain ${favoriteColor}! Fighting for justice and style.`);
+
+let num1 = 4;
+let num2 = 8;
+
+console.log("Addition:", num1 + num2);
+console.log("Subtraktion:", num1 - num2);
+console.log("Multiplikation:", num1 * num2);
+console.log("Division:", num1/num2);
