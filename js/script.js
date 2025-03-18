36057177ccb1e18429767001418d9edc34e9703c
Idace <idaebeling@gmail.com>
2025-03-18 18:04:12 +0100

    Changed text+num1

--- js/script.js
+++ js/script.js
@@ -7,9 +7,8 @@ let favoriteColor="green";
 console.log(`By day, I'm just ${name}, an ordinary ${age} -year old. But when the sun sets... I become the Captain ${favoriteColor}! Fighting for justice and style.`);
 
 let num1 = 4;
-let num2 = 8;
 
-console.log("Addition:", num1 + num2);
-console.log("Subtraktion:", num1 - num2);
-console.log("Multiplikation:", num1 * num2);
-console.log("Division:", num1/num2);
+console.log("Addition:", num1 + 8);
+console.log("Subtraktion:", num1 - 6);
+console.log("Multiplikation:", num1 * 3);
+console.log("Division:", num1/10);
