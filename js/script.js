3b1d9a25bd2c974b803d99b465e5030206c16faf
Idace <idaebeling@gmail.com>
2025-03-18 18:09:03 +0100

    changed to price

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ let favoriteColor="green";
 
 console.log(`By day, I'm just ${name}, an ordinary ${age} -year old. But when the sun sets... I become the Captain ${favoriteColor}! Fighting for justice and style.`);
 
-let num1 = 4;
+let price = 4;
 
-console.log("Addition:", num1 + 8);
-console.log("Subtraktion:", num1 - 6);
-console.log("Multiplikation:", num1 * 3);
-console.log("Division:", num1/10);
+console.log("Addition:", price + 8);
+console.log("Subtraktion:", price - 6);
+console.log("Multiplikation:", price * 3);
+console.log("Division:", price/10);
